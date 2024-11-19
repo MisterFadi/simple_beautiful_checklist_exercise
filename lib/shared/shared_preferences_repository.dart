e559cd8695478c947ba34b7f148ce91b31a0bc05
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-11-19 16:16:05 +0100

    add preferences

--- lib/shared/shared_preferences_repository.dart
+++ lib/shared/shared_preferences_repository.dart
@@ -3,35 +3,43 @@ import 'package:simple_beautiful_checklist_exercise/shared/database_repository.d
 
 class SharedPreferencesRepository implements DatabaseRepository {
   final SharedPreferencesAsync prefs = SharedPreferencesAsync();
-
   List<String> _items = [];
 
+  // Hinzufügen und Speichern
   @override
   Future<void> addItem(String item) async {
     _items.add(item);
-    await _saveItems();
   }
 
+// Löschen und das gelöschte speichern
   @override
-  Future<void> deleteItem(int index) {
-    throw UnimplementedError();
+  Future<void> deleteItem(int index) async {
+    _items.removeAt(index);
   }
 
+// Bearbeiten und das bearbeitete speichern
   @override
-  Future<void> editItem(int index, String newItem) {
-    throw UnimplementedError();
+  Future<void> editItem(int index, String newItem) async {
+    getItems();
+    _items[index] = newItem;
   }
 
+// Zeigt alles was gespeichert wurde in einer Liste
   @override
   Future<List<String>> getItems() async {
-    _items = await prefs.getStringList("Hallo") ?? [];
+    _items = await prefs.getStringList("Aufgaben") ?? [];
     return _items;
   }
 
+// Zeigt unter Statistik an wie viele Einträge gespeichert wurden.
   @override
-  Future<int> get itemCount => throw UnimplementedError();
+  Future<int> get itemCount async {
+    await getItems();
+    return _items.length;
+  }
 
-  Future<void> _saveItems() async {
-    await prefs.setStringList("Hallo", _items);
+  // Speichert die Funktion saveItem
+  Future<void> _saveItem() async {
+    await prefs.setStringList("Aufgaben", _items);
   }
 }
